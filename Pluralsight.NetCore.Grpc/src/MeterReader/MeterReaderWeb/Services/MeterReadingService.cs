79a5ad19f1c87d5d1a1e97c9a651ddf66eb267f2
Denis Prokhorchik <live-dev@hotmail.com>
2022-06-13 14:06:54 +0300

    feat:  add meterreaderservice.cs

---
+++ Pluralsight.NetCore.Grpc/src/MeterReader/MeterReaderWeb/Services/MeterReadingService.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+using MeterReader.gRPC;
+using MeterReaderWeb.Data;
+using MeterReaderWeb.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using static MeterReader.gRPC.MeterReaderService;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterReadingService : MeterReaderServiceBase
+    {
+        private readonly IReadingRepository _repository;
+        private readonly ILogger<MeterReadingService> _logger;
+
+        public MeterReadingService(IReadingRepository repository, I`Logger<MeterReadingService> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            if (request.Successful == ReadingStatus.Success)
+            {
+                foreach (var reading in request.Redings)
+                {
+                    var readingValue = new MeterReading()
+                    {
+                        CustomerId = reading.CustomerId,
+                        Value = reading.ReadingValue,
+                        ReadingDate = reading.ReadingTime.ToDateTime(),
+                    };
+
+                    _repository.AddEntity(readingValue);
+                }
+            }
+
+            if (await _repository.SaveAllAsync())
+            {
+                return new StatusMessage()
+                {
+                    //Notes = "tesss",
+                    Success= ReadingStatus.Success
+                };
+            }
+
+             return new StatusMessage()
+                {
+                    //Notes = "Failed",
+                    Success= ReadingStatus.Failure
+                };
+        }
+    }
+}
